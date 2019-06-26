a6a29e8b8ee7051cb7adaa1eee45331ff56ccf25
Rocco Swat <nestorsuat@gmail.com>
2019-06-25 20:37:55 -0500

    Carga tweets que ya empezaron

--- accident-tagger/api/controllers/post/get-post.js
+++ accident-tagger/api/controllers/post/get-post.js
@@ -25,7 +25,7 @@ module.exports = {
   },
 
   fn: async function (inputs, exits) {
-    if(!this.req.me && !inputs.username) {
+    if (!this.req.me && !inputs.username) {
       return exits.success({
         id: 0,
         // Antes eso se hacía para usuarios no logueados pudieran etiquetar, esta caracteristicas cambio para solo usuarios registrados
@@ -33,8 +33,8 @@ module.exports = {
         text: 'Para comenzar inicie sesión con un usuario y contraseña valida, o por favor registrese con nosotros, reconocer su identidad es muy útil para nosotros.'
       });
     }
-    if(this.req.me){
-      if(this.req.me.emailStatus == 'unconfirmed'){
+    if (this.req.me) {
+      if (this.req.me.emailStatus == 'unconfirmed') {
         return exits.success({
           id: -2,
           text: 'Por favor confirme su correo electronico para comenzar a etiquetar, reconocer su identidad es muy útil para nosotros.'
@@ -45,20 +45,35 @@ module.exports = {
     var record = undefined;
     var flat = false;
 
-    var postUserTags = [];
-    var username = this.req.me? this.req.me.emailAddress : inputs.username;
-    if(username){
-      var userTags = await Score.find({username: username}).select(['post']);
-      userTags.forEach((i)=>{
-        postUserTags.push(i.post)
+    var postUserTagsMe = [];
+    var postUserTagsOther = [];
+    var username = this.req.me ? this.req.me.emailAddress : inputs.username;
+    if (username) {
+      var userTagsMe = await Score.find({username: username}).select(['post']);
+      userTagsMe.forEach((i) => {
+        postUserTagsMe.push(i.post)
       });
-
+      var userTagsOther = await Score.find({username: {"!=": username}}).select(['post']);
+      userTagsOther.forEach((i) => {
+        postUserTagsOther.push(i.post)
+      });
     }
-    var max = await Post.count({id: { nin: postUserTags},complete:0}); //Post que no hallan superado la cantidad máxima de votaciones --> complete:0
-    let skipRandom = Math.floor(Math.random()*max);
-    record = await Post.find({id: { nin: postUserTags},complete:0}).skip(skipRandom).limit(1);
 
-    record = record[0]; //Porque el record guarda el resultado de Post.find y no Post.findOne por lo que es un arreglo
+    let difference = postUserTagsOther.filter(x => !postUserTagsMe.includes(x));
+    let unique = difference.filter((value, index, self) => self.indexOf(value) === index);
+
+    var max = await Post.count({id: { in: unique},complete:0}); //Post que no hallan superado la cantidad máxima de votaciones --> complete:0
+    var skipRandom = Math.floor(Math.random()*max);
+    record = await Post.find({id: { in: unique},complete:0}).skip(skipRandom).limit(1);
+
+    record = record[0];
+    if (!record){
+      max = await Post.count({id: { nin: postUserTagsMe},complete:0}); //Post que no hallan superado la cantidad máxima de votaciones --> complete:0
+      skipRandom = Math.floor(Math.random()*max);
+      record = await Post.find({id: { nin: postUserTagsMe},complete:0}).skip(skipRandom).limit(1);
+      record = record[0]; //Porque el record guarda el resultado de Post.find y no Post.findOne por lo que es un arreglo
+    }
+
     if (!record){
       record = {
         id: -1,

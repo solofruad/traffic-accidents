41e1ffee7936300bf83229726b019d7b627866dd
Rocco Swat <nestorsuat@gmail.com>
2020-05-22 22:27:42 -0500

    Cambios y otros

--- model/untitled2.py
+++ model/untitled2.py
@@ -95,11 +95,20 @@ def preText(text):
 preText("rt hola... rt art articulo rto rt")
 preText("RT ArTiculo #EsteEsUnEjemplo #EsMás RT")
 
-
+    
+        
 brief_cleaning = (preText(str(row)) for row in X)
 txt = [clean_fn(doc) for doc in nlp.pipe(brief_cleaning, n_threads=-1)]
 
+train['text'] = txt
 
+brief_cleaning = (preText(str(row)) for row in X_test)
+txt = [clean_fn(doc) for doc in nlp.pipe(brief_cleaning, n_threads=-1)]
+
+test['text'] = txt
+
+train.to_csv("data/v1/7030/train70clean.tsv",sep='\t')
+test.to_csv("data/v1/7030/test30clean.tsv",sep='\t')
 #El RT lo reemplazo en la función clean_fn
 
 #Falta:
@@ -114,16 +123,63 @@ txt = [clean_fn(doc) for doc in nlp.pipe(brief_cleaning, n_threads=-1)]
 
 
 
+arr = pd.read_csv("test30beto.csv")
+
+
+
+arr.iloc[0][0]
 
 
 
 
 
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.model_selection import StratifiedKFold
+from sklearn import model_selection
+
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import f1_score
 
 
+train = pd.read_csv("data/v1/7030/train70beto.csv")
+test = pd.read_csv("data/v1/7030/test30beto.csv")
 
+y_train = train['label']
+del train['label']
+X_train = train.values
 
+y_test = test['label']
+del test['label']
+X_test = test.values
 
+#Model SVM
+C = 0.1
+gamma = 0.05
+kernel = 'poly'
 
+classifier = SVC(random_state=123, kernel=kernel, gamma=gamma, C=C)
+classifier.fit(X_train, y_train)
 
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+cm_svm = confusion_matrix(y_test, y_pred)
+
+metrics_svm = []
+metrics = {}
+metrics['accuracy'] = accuracy_score(y_test, y_pred)
+metrics['recall'] = recall_score(y_test, y_pred)
+metrics['precision'] = precision_score(y_test, y_pred)
+metrics['f1'] = f1_score(y_test, y_pred)
+metrics_svm.append(metrics)
+metrics_svm = pd.DataFrame(metrics_svm)
+
+print(metrics_svm)
+print(cm_svm)
 
